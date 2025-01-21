92149624e2a82ca665a203c2871ac5918d9b34c0
Mridul Bansal <bansalmridul691@gmail.com>
2025-01-21 23:49:59 +0530

    added show and hide functionality to library system

--- library/script.js
+++ library/script.js
@@ -2,18 +2,16 @@ function newBook(title,author)
 {
     this.title=title;
     this.author=author;}
-let q=new newBook("Harry Potter","JK");
+
 let lib=[];
 
-lib.push(q);
-lib.push(new newBook("ABC", "123"));
 function show(){
 const table=document.getElementById("table");
 table.innerHTML="";
 for(let i=0; i<lib.length; i++)
 {
     let nr=document.createElement("tr");
-    nr.innerHTML="<td>"+lib[i].title+"</td><td>"+lib[i].author+"</td>";
+    nr.innerHTML="<td>"+(i+1)+"</td><td>"+lib[i].title+"</td><td>"+lib[i].author+"</td>";
     table.append(nr);
 
 }}
@@ -22,5 +20,14 @@ form.addEventListener('submit', function(event){
     event.preventDefault();
     const ff=new FormData(form);
     lib.push(new newBook(ff.get("bookName"),ff.get("author")));
+   form.reset();
+})
+const sho=document.getElementById("showBooks");
+sho.addEventListener('click',function(){
     show();
+})
+const hB=document.getElementById("hide");
+hB.addEventListener('click',function(){
+    const table=document.getElementById("table");
+table.innerHTML="";
 })
